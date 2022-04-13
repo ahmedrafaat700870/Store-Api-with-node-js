c418a2a9bebcf340f49614be201d4d5739e7a0be
Ahmed Rafaat <71382395+ahmedrafaat700870@users.noreply.github.com>
2022-04-13 09:10:05 +0200

    update authentication

--- src/routes/api/Product.routes.ts
+++ src/routes/api/Product.routes.ts
@@ -2,8 +2,8 @@ import * as controller from '../../controller/Product.controller'
 import authanticateUser from '../../middleware/authanticate.middleware'
 import { Router } from 'express'
 const ProductRoutes = Router()
-ProductRoutes.get('/GetAll', authanticateUser, controller.GetAll)
-  .get('/GetOne/:id', authanticateUser, controller.GetOne)
+ProductRoutes.get('/GetAll', controller.GetAll)
+  .get('/GetOne/:id', controller.GetOne)
   .post('/Create', authanticateUser, controller.Create)
   .delete('/Delete', authanticateUser, controller.DeleteOne)
   .patch('/UpdateOne', authanticateUser, controller.UpdataOne)
