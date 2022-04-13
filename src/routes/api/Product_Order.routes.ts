11925a7687ca18f3143ba831c6b3b1a1982b80e6
Ahmed Rafaat <71382395+ahmedrafaat700870@users.noreply.github.com>
2022-04-13 18:56:58 +0200

    update authenticate routes in product order

--- src/routes/api/Product_Order.routes.ts
+++ src/routes/api/Product_Order.routes.ts
@@ -2,8 +2,8 @@ import * as controller from '../../controller/Product_Order.controller'
 import authanticateUser from '../../middleware/authanticate.middleware'
 import { Router } from 'express'
 const ProductOrderRoutes = Router()
-ProductOrderRoutes.get('/getOne/:id', controller.GetOne)
-  .get('/GetOrders', controller.GetAll)
+ProductOrderRoutes.get('/getOne/:id', authanticateUser, controller.GetOne)
+  .get('/GetOrders', authanticateUser, controller.GetAll)
   .post('/CreateOrder', authanticateUser, controller.CreateOne)
   .patch('/UpdateOrder', authanticateUser, controller.Update)
   .delete('/DeleteOrder', authanticateUser, controller.Delete)
